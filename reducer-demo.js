ef9285fc8f08b7ac247612bc35077d9fca1f9318
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-24 09:02:01 +0300

    reducer in a nutshell

--- reducer-demo.js
+++ reducer-demo.js
@@ -1,5 +1,31 @@
-console.log("demo in action");
-
+// console.log("demo in action");
 const redux = require("redux");
 
-const store = redux.createStore();
+// reducer funkcija
+const counterReducer = (state = { counter: 0 }, action) => {
+  //   console.log("inside reducer fn ", state.counter);
+  if (action.type === "INCR") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DECR") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
+
+// sukuriam viena tiesos saltini
+const store = redux.createStore(counterReducer);
+
+// prisijungimo funkcija
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log("subscriber was executed", latestState);
+};
+
+// visi failai kurie nori naudoti store turi prisijungti su subscibe
+store.subscribe(counterSubscriber);
+
+// ivygdom reducerio funkcija su dispatch
+store.dispatch({ type: "INCR" });
+store.dispatch({ type: "INCR" });
+store.dispatch({ type: "DECR" });
